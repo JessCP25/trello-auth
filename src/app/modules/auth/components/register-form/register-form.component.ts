7bd72433bccbd0f363566ba644f8ac92baeccce9
JessCP25 <jpayanoc25@gmail.com>
2024-11-25 22:05:55 -0500

    Feat: Validando existencia de usuario

--- src/app/modules/auth/components/register-form/register-form.component.ts
+++ src/app/modules/auth/components/register-form/register-form.component.ts
@@ -12,18 +12,29 @@ import { CustomValidators } from '@utils/validators';
   templateUrl: './register-form.component.html',
 })
 export class RegisterFormComponent {
-  form = this.formBuilder.nonNullable.group({
-    name: ['', [Validators.required]],
+  formUser = this.formBuilder.nonNullable.group({
     email: ['', [Validators.email, Validators.required]],
-    password: ['', [Validators.minLength(8), Validators.required]],
-    confirmPassword: ['', [Validators.required]],
-  }, {
-    validators: [ CustomValidators.MatchValidator('password', 'confirmPassword') ]
   });
+
+  form = this.formBuilder.nonNullable.group(
+    {
+      name: ['', [Validators.required]],
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', [Validators.minLength(8), Validators.required]],
+      confirmPassword: ['', [Validators.required]],
+    },
+    {
+      validators: [
+        CustomValidators.MatchValidator('password', 'confirmPassword'),
+      ],
+    }
+  );
   status: RequestStatus = 'init';
+  statusUser: RequestStatus = 'init';
   faEye = faEye;
   faEyeSlash = faEyeSlash;
   showPassword = false;
+  showRegister = false;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -37,16 +48,42 @@ export class RegisterFormComponent {
       const { name, email, password } = this.form.getRawValue();
       console.log(name, email, password);
       this.authService.register(name, email, password).subscribe({
-        next: () =>{
+        next: () => {
           this.status = 'success';
           this.router.navigate(['/login']);
         },
-        error: () =>{
-          this.status = 'failed'
-        }
-      })
+        error: () => {
+          this.status = 'failed';
+        },
+      });
     } else {
       this.form.markAllAsTouched();
+    }
+  }
+
+  validateUser() {
+    if (this.formUser.valid) {
+      this.statusUser = 'loading';
+      const { email } = this.formUser.getRawValue();
+      this.authService.isAvailable(email).subscribe({
+        next: (rta) => {
+          this.statusUser = 'success';
+          if (rta.isAvailable) {
+            this.showRegister = true;
+            this.form.controls.email.setValue(email);
+          } else {
+            this.router.navigate(['/login'], {
+              queryParams: {email}
+            });
+          }
+        },
+        error: (error) => {
+          this.statusUser = 'failed';
+          console.log(error);
+        },
+      });
+    } else {
+      this.formUser.markAllAsTouched();
     }
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,23 +3,34 @@ import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = environment.API_URL
+  private apiUrl = environment.API_URL;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  login(email: string, password: string){
+  login(email: string, password: string) {
     return this.http.post(`${this.apiUrl}/api/v1/auth/login`, {
-      email, password
-    })
+      email,
+      password,
+    });
   }
 
-  register(name: string, email: string, password: string){
+  register(name: string, email: string, password: string) {
     return this.http.post(`${this.apiUrl}/api/v1/auth/register`, {
-      name, email, password
-    })
+      name,
+      email,
+      password,
+    });
   }
 
+  isAvailable(email: string) {
+    return this.http.post<{ isAvailable: boolean }>(
+      `${this.apiUrl}/api/v1/auth/is-available`,
+      {
+        email,
+      }
+    );
+  }
 }
