32f850c7e43fe591c43db2c3a87d41c65cd9cdee
JessCP25 <jpayanoc25@gmail.com>
2024-12-05 23:18:21 -0500

    Feat: Implementar redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RedirectGuard } from '@guards/redirect.guard';
 import { AuthGuard } from '@guards/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
+    canActivate: [RedirectGuard],
     loadChildren: () => import('./modules/auth/auth.module').then((m) => m.AuthModule),
   },
   {
--- src/app/guards/redirect.guard.ts
+++ src/app/guards/redirect.guard.ts
@@ -2,13 +2,12 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { TokenService } from '@services/token.service';
 
-export const AuthGuard: CanActivateFn = () => {
+export const RedirectGuard: CanActivateFn = () => {
   const token = inject(TokenService).getToken();
   const router = inject(Router);
 
-  if (!token) {
-    router.navigate(['/login']);
-    return false;
+  if (token) {
+    router.navigate(['/app']);
   }
   return true;
 };
